955b1568a8530fd124745a282173ce6d2b1a2953
shuxian <shuxian@huize.com>
2018-03-07 10:22:23 +0800

    改成part1分支

--- block.go
+++ block.go
@@ -3,24 +3,9 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"fmt"
 	"strconv"
 	"time"
 )
-
-func main() {
-	bc := NewBlockchain()
-
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
-}
 
 // Block 简单的区块
 // 先从“区块链”中的“区块”说起。在区块链中，块存储了变量信息，比如，比特币的区块存储了交易、还有加密货币
@@ -57,35 +42,8 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	return block
 }
 
-/**
-实现一个简单的区块链，区块链的本质就是一个一定结构的数据库：
-   有序的、首尾相连的链状列表。即是，区块们都是顺序、每一个块都连接着前面的一个块。
-   这个结构使得可以在区块链中快速找到最后一个区块，尤其是可以通过hash值找到区块
-在golang里可以使用数组、map来实现，数组可以保证顺序，map实现hash->block组合的映射
-不过，针对目前的进度，我们不需要实现能过hash找到区块的方法，所以这里只用数组来保证顺序即可。
-**/
-
-// Blockchain 区块链结构
-// 这就是一个简单的区块链了
-type Blockchain struct {
-	blocks []*Block
-}
-
-// AddBlock 给区块链添加增加区块的能力
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
-}
-
 // NewGenesisBlock 创世区块
 // 为了创建新的区块，需要一个已经存在的区块，但是现在还没有任何一个区块。而在区块链中，第一个区块，就是“创世区块”。
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
-}
-
-// NewBlockchain 构建新区块链
-// 使用创世区块来引导区块链
-func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
