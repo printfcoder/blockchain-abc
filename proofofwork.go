4841355917b9c5f579f129b6fd501daeb4abdfe5
shuxian <shuxian@huize.com>
2018-03-09 09:55:23 +0800

    part_3 持久化

--- proofofwork.go
+++ proofofwork.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"math"
 	"math/big"
-	"time"
 )
 
 var (
@@ -15,13 +14,13 @@ var (
 
 const targetBits = 24
 
-// ProofOfWork 工作量证明结构
+// ProofOfWork represents a proof-of-work
 type ProofOfWork struct {
 	block  *Block
 	target *big.Int
 }
 
-// NewProofOfWork 新建工作证明
+// NewProofOfWork builds and returns a ProofOfWork
 func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-targetBits))
@@ -46,20 +45,18 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	return data
 }
 
-// Run 执行PoW实际上的计算
+// Run performs a proof-of-work
 func (pow *ProofOfWork) Run() (int, []byte) {
 	var hashInt big.Int
 	var hash [32]byte
 	nonce := 0
-	start := time.Now()
-	fmt.Println("start:", start)
+
 	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
-	fmt.Print("\n")
 	for nonce < maxNonce {
 		data := pow.prepareData(nonce)
 
 		hash = sha256.Sum256(data)
-		// fmt 可能会阻塞，造成总体计算过慢，可选择不打印 fmt.Printf("\r%x", hash)
+		fmt.Printf("\r%x", hash)
 		hashInt.SetBytes(hash[:])
 
 		if hashInt.Cmp(pow.target) == -1 {
@@ -68,17 +65,12 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 			nonce++
 		}
 	}
-
-	fmt.Printf("\r%x\n", hash)
-	elapsed := time.Since(start)
-	fmt.Println("end: ", time.Now())
-	fmt.Println("elapsed time: ", elapsed)
-
 	fmt.Print("\n\n")
+
 	return nonce, hash[:]
 }
 
-// Validate 难PoW是否合法
+// Validate validates block's PoW
 func (pow *ProofOfWork) Validate() bool {
 	var hashInt big.Int
 
